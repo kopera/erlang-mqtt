114ba00918d73f8208a8f90aacf6cb2211bece06
Ali Sabil <ali.sabil@gmail.com>
2017-03-10 12:36:15 +0530

    Release 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,8 +3,34 @@ defmodule MQTT.Mixfile do
 
   def project do
     [app: :mqtt,
-     description: "MQTT protocol implementation",
-     version: "0.2.0",
-     elixir: "~> 1.0"]
+     name: "MQTT",
+     version: "0.1.0",
+     elixir: "~> 1.0",
+     package: package(),
+     description: "Erlang/Elixir low level MQTT protocol implementation",
+     deps: deps()]
+  end
+
+  defp package() do
+    [maintainers: ["Ali Sabil"],
+     files: [
+       "include",
+       "lib",
+       "LICENSE*",
+       "mix.exs",
+       "README*",
+       "rebar.config",
+       "rebar.lock",
+       "src"
+     ],
+     build_tools: ["mix", "rebar3"],
+     licenses: ["Apache 2.0"],
+     links: %{"Github" => "https://github.com/kopera/erlang-mqtt"}]
+  end
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.15.0", only: :dev},
+    ]
   end
 end
