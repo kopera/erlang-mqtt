55c30c7dadc648e60b344a2a3107dd6083ec1f56
Ali Sabil <ali.sabil@gmail.com>
2017-03-21 20:59:19 +0100

    Release 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MQTT.Mixfile do
   def project do
     [app: :mqtt,
      name: "MQTT",
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.0",
      package: package(),
      description: "Erlang/Elixir low level MQTT protocol implementation",
