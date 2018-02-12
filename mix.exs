aaf9df954922e50e017d839129162952d319d654
Ali Sabil <ali.sabil@gmail.com>
2018-02-12 14:37:55 +0100

    Update the Elixir bindings for mqtt_client

--- mix.exs
+++ mix.exs
@@ -2,31 +2,42 @@ defmodule MQTT.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :mqtt,
-     name: "MQTT",
-     version: "0.2.3",
-     elixir: "~> 1.0",
-     package: package(),
-     description: "Erlang/Elixir low level MQTT protocol implementation",
-     source_url: "https://github.com/kopera/erlang-mqtt",
-     deps: deps()]
+    [
+      app: :mqtt,
+      name: "MQTT",
+      version: "0.2.3",
+      elixir: "~> 1.0",
+      package: package(),
+      description: "Erlang/Elixir low level MQTT protocol implementation",
+      source_url: "https://github.com/kopera/erlang-mqtt",
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      mod: {:mqtt_app, []},
+      registered: [:mqtt_sup, :mqtt_client_sup],
+    ]
   end
 
   defp package() do
-    [maintainers: ["Ali Sabil"],
-     files: [
-       "include",
-       "lib",
-       "LICENSE*",
-       "mix.exs",
-       "README*",
-       "rebar.config",
-       "rebar.lock",
-       "src"
-     ],
-     build_tools: ["mix", "rebar3"],
-     licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/kopera/erlang-mqtt"}]
+    [
+      maintainers: ["Ali Sabil"],
+      files: [
+        "include",
+        "lib",
+        "LICENSE*",
+        "mix.exs",
+        "README*",
+        "rebar.config",
+        "rebar.lock",
+        "src"
+      ],
+      build_tools: ["mix", "rebar3"],
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/kopera/erlang-mqtt"}
+    ]
   end
 
   defp deps do
--- test/client/interaction_test.exs
+++ test/client/interaction_test.exs
@@ -3,52 +3,43 @@ defmodule MQTT.Client.IntegrationTest do
   @moduledoc false
 
   use ExUnit.Case
-  alias MQTT.TestClient
+  alias MQTT.Client
 
   @tag :external
   test "Client should handle publish and subscribe" do
-    Process.register self(), :test_client
-
-    {:ok, pid} = TestClient.start_link(
-      %{:transport => {:tcp,
-          %{:host => "localhost"}},
-      })
-    assert_receive {:connected, false}
+    {:ok, connection} = Client.connect(%{
+      transport: {:tcp, %{host: "localhost"}}
+    })
 
     topic = "/my/data/topic"
 
-    TestClient.subscribe(pid, topic)
-    assert_receive {:subscribed, ^topic}
-
-    TestClient.publish(pid, topic, "Hello")
-    assert_receive {:publish, ^topic, "Hello"}
-
+    {:ok, [{^topic, 0}]} = Client.subscribe(connection, topic)
+    :ok = Client.publish(connection, topic, "Hello")
   end
 
   @tag :external
   test "Client should support Last Will and Testament" do
-    Process.register self(), :test_client
-
     lwt_topic = "/last/will"
-    {:ok, pid1} = TestClient.start_link(%{
-        last_will: %{
-          topic: lwt_topic,
-          message: "Goodbye cruel World!",
-          qos: 0,
-          retain: false
-        }
+    lwt_message = "Goodbye cruel World!"
+
+    {:ok, connection1} = Client.connect(%{
+      transport: {:tcp, %{host: "localhost"}},
+      last_will: %{
+        topic: lwt_topic,
+        message: lwt_message,
+        qos: 0,
+        retain: false
+      }
     })
-    assert_receive {:connected, false}
 
-    {:ok, pid2} = TestClient.start_link(%{})
-    assert_receive {:connected, false}
+    {:ok, connection2} = Client.connect(%{
+      transport: {:tcp, %{host: "localhost"}}
+    })
 
-    TestClient.subscribe(pid2, lwt_topic)
-    assert_receive {:subscribed, ^lwt_topic}
+    {ok, [{lwt_topic, _}]} = Client.subscribe(connection2, lwt_topic)
+    :gen_fsm.stop(connection1)
 
-    :gen_fsm.stop(pid1)
-
-    assert_receive {:publish, ^lwt_topic, "Goodbye cruel World!"}
+    assert_receive {:mqtt_client, ^connection2, {:publish, ^lwt_topic, ^lwt_message}}
 
   end
 
